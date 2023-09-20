e113e5128298741c73845ae3033edd505ac94317
Aiden <x29587629@gmail.com>
2023-09-20 21:30:49 +0800

    first commit

---
+++ src/test/java/com/cooba/webauthn/WebauthnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooba.webauthn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebauthnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
